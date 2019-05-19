74297a068211fd82f17e9e361ae6548589235d6c
dhkim0800 <dhkim0800@noreply.github.com>
2019-05-19 17:50:42 +0900

    Refactor toggle

--- src/main/Drawer.java
+++ src/main/Drawer.java
@@ -4,5 +4,5 @@ import java.awt.Graphics2D;
 
 public abstract class Drawer {
 	public abstract void draw(Graphics2D g2, int width, int height);
-	public abstract void toggle(boolean tg);
+	public abstract void toggle(boolean newMode);
 }
